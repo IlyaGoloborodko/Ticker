e9ec31c4310f8251c49c77ac3f01d54ddb737201
Ilya <sok203333@gmail.com>
2023-12-05 21:27:42 +0300

    Changed DEBUG_PROPAGATE_EXCEPTIONS = True

--- projectx/projectx/settings.py
+++ projectx/projectx/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pskh-n1#23vc@g$mlskmcot3yo@zp@ky_2si7fo=6j36ix)%*#
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # Application definition
 
