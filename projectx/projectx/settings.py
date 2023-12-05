fca6f0ec9c46961e5ff36964265fca0fdabaa2f9
Ilya <sok203333@gmail.com>
2023-12-05 20:59:17 +0300

    Changed allowed hosts to *

--- projectx/projectx/settings.py
+++ projectx/projectx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pskh-n1#23vc@g$mlskmcot3yo@zp@ky_2si7fo=6j36ix)%*#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
